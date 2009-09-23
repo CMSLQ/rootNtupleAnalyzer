2e2dbf1c907557831e077b1e41d880cdc559e14c
temple <>
2009-09-23 21:42:51 +0000

    Added optimization code, along with a histogram showing number of events vs. cut

--- include/baseClass.h
+++ include/baseClass.h
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <iomanip>
 #include <TH1F.h>
+#include <TH1I.h>
 #include <math.h>
 
 #define STDOUT(STRING) {		   \
@@ -55,12 +56,68 @@ struct preCut {
   string level_str;
 };
 
+// Create structure to hold 
+class Optimize {
+ public:
+  Optimize(){count=0; variableName=""; minvalue=0; maxvalue=0; testgreater=false; level_int=-10;};
+  Optimize(int x0, string x1, double x2, double x3, bool x4, int x5)
+    {
+      count=x0;
+      variableName=x1;
+      minvalue=x2;
+      maxvalue=x3;
+      if (minvalue>maxvalue)
+	{
+	  maxvalue=x2;
+	  minvalue=x3;
+	}
+      increment=(maxvalue-minvalue)/9.;
+      if (increment<=0)
+	increment=1;
+      testgreater=x4;
+      level_int=x5;
+      value=-999999; // dummy start value
+    };
+  ~Optimize(){};
+  int count; // store number for ordering of optimization cuts
+  string variableName; // store name of variable
+  double minvalue; // minimum threshold value to test
+  double maxvalue; // maximum threshold to test
+  double increment; // max-min, divided into 10 parts
+  bool testgreater; // tests whether value should be greater or less than threshold
+  int level_int; // cut level -- not used?
+  double value;  // value to check against threshold
+
+  bool Compare(int counter)
+    {
+      // compare value to threshold # <counter>
+      
+      // if testing that value is greater than some threshold, start with lowest threshold first
+      bool passed=false;
+      if (testgreater)
+	{
+	  double thresh=minvalue+increment*counter; // convert counter # to physical threshold
+	  value > thresh ? passed=true: passed=false;
+	}
+      // if testing that value is less than threshold, start with largest threshold first.  This keep the number of events "monotonically decreasing" over a series of 10 cuts.
+      else
+	{
+	  double thresh=maxvalue-increment*counter;
+	  value < thresh ? passed=true : passed = false;
+	}
+      return passed;
+    }; // comparison function
+}; // class Optimize
+
+
 class baseClass : public rootNtupleClass {
   public :
   map<string, bool> combCutName_passed_;
 
   void baseClass::resetCuts();
   void fillVariableWithValue(const std::string&, const double&);
+  void fillOptimizerWithValue(const string& s, const double& d);
+  void runOptimizer();
   void baseClass::evaluateCuts();
   bool baseClass::passedCut(const string& s);
   bool baseClass::passedAllPreviousCuts(const string& s);
@@ -103,6 +160,9 @@ class baseClass : public rootNtupleClass {
   map<string, preCut> preCutName_cut_;
   map<string, cut> cutName_cut_;
   vector<string> orderedCutNames_; 
+  map<int, Optimize> optimizeName_cut_;
+  TH1F* h_optimizer_;
+  TH1F* eventcuts_;
   void baseClass::init();
   void baseClass::readInputList();
   void baseClass::readCutFile();
